3c52cbd5b0af0cc27eee18f9c29c16925cb22117
suin0704 <fropop74@gmail.com>
2023-09-08 09:24:14 +0900

    풀캘린더 컨트롤러 다시

--- src/main/java/kr/co/groovy/schedule/ScheduleController.java
+++ src/main/java/kr/co/groovy/schedule/ScheduleController.java
@@ -69,6 +69,96 @@ public class ScheduleController {
 			log.info("jsonArrCheck:{}",jsonArr);
 			return jsonArr;
 		}
-
-	}
-
+	
+	 	
+	 	
+	 	@GetMapping("calendar-admin-update/{schdulSn}")
+	 	public ResponseEntity<ScheduleVO> findOneSchedule(@PathVariable int schdulSn) {
+	 		ScheduleVO scheduleVO = scheduleService.getOneSchedule(schdulSn);
+	 		
+	 		 if (scheduleVO != null) {
+	 		        return new ResponseEntity<>(scheduleVO, HttpStatus.OK);
+	 		    } else {
+	 		        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	 		    }
+	 
+	 	}
+	 		
+	 	@PostMapping("calendar-admin-update")
+	 	@ResponseBody
+	 	public String insertSchedule(@RequestBody List<Map<String, Object>> param) throws ParseException, Exception {
+	 	    
+	 		 DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.KOREA);
+	 		
+	 	    for (Map<String, Object> list : param) {
+	 	        
+	 	        String title = (String) list.get("title");
+	 	        String start = (String) list.get("start");
+	 	        String end = (String) list.get("end");
+	 	        
+	 	        LocalDateTime startDateUTC = LocalDateTime.parse(start, dateTimeFormatter);
+	 	        LocalDateTime endDateUTC = LocalDateTime.parse(end, dateTimeFormatter);
+	 	        
+	 	        LocalDateTime startDate = startDateUTC.plusHours(9);
+	 	        LocalDateTime endDate = endDateUTC.plusHours(9);
+	 	        
+	 	        ScheduleVO scheduleVO = new ScheduleVO();
+	 	        
+	 	        scheduleVO.setSchdulNm(title);
+	 
+	 	        Date startDateAsDate = Date.from(startDate.atZone(ZoneId.systemDefault()).toInstant());
+	 	        scheduleVO.setSchdulBeginDate(startDateAsDate);
+	 	        Date endDateAsDate = Date.from(endDate.atZone(ZoneId.systemDefault()).toInstant());
+	 	        scheduleVO.setSchdulClosDate(endDateAsDate);
+	 
+	 	        
+	 	        scheduleService.inputSchedule(scheduleVO);
+	 	    }
+	 	    
+	 	    return "/full-calendar/main";
+	 	}
+	 	
+	 	
+	 	
+	 	@PutMapping("calendar-admin-update")
+	 	@ResponseBody
+	 	public String modifySchedule(@RequestBody List<Map<String, Object>> param) throws ParseException, Exception {
+	 		
+	 		int result = 0;
+	 		
+	 		for(Map<String, Object> list : param) {
+	 			
+	 			String id = (String) list.get("id"); 
+	 			String title = (String) list.get("title");
+	 			String start = (String) list.get("start");
+	 			String end = (String) list.get("end");
+	 			
+	 			ScheduleVO scheduleVO = new ScheduleVO();
+	 			
+	 			Integer idInt = Integer.valueOf(id);
+	 			scheduleVO.setSchdulSn(idInt);
+	 			
+	 			scheduleVO.setSchdulNm(title);
+	 			
+	 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	 
+	 			Date startdate = dateFormat.parse(start);
+	 			Date enddate = dateFormat.parse(end);
+	 
+	 			scheduleVO.setSchdulBeginDate(startdate);
+	 			scheduleVO.setSchdulClosDate(enddate);
+	 	        
+	 	        scheduleService.modifySchedule(scheduleVO);
+	 		}
+	 		
+	 		
+	 		if (result>0) {
+	 			return "/full-calendar/main";
+	 		} else {
+	 			return "/full-calendar/calendar-admin-update";
+	 		}
+	 		
+	 	}
+	 		 	
+}
+	 
