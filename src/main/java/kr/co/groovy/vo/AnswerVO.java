39a99cf033b2b2f8844b32b598613cf266387bc4
Yun-HaNeul <qnrty12@gmail.com>
2023-09-10 16:06:29 +0900

    answerVO

---
+++ src/main/java/kr/co/groovy/vo/AnswerVO.java
@@ -0,0 +1,13 @@
+package kr.co.groovy.vo;
+
+
+import java.sql.Date;
+
+public class AnswerVO {
+    private int answerNo;
+    private String sntncEtprCode;
+    private String answerWrtingEmplId;
+    private String answerCn;
+    private Date answerDate;
+
+}
